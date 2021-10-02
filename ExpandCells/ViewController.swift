7ea0439dd34f9bb10cb385ad93644a431d6e7808
2rajeshevv <2rajeshevv@gmail.com>
2021-10-03 01:01:38 +0530

    first initial commit of expanding and collapsing table view cells

--- ExpandCells/ViewController.swift
+++ ExpandCells/ViewController.swift
@@ -7,13 +7,92 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class Section {
+    
+    let title: String
+    let data: [String]
+    var isOpened: Bool = false
+    
+    init(title: String, data: [String], isOpened: Bool = false) {
+        self.title = title
+        self.data = data
+        self.isOpened = isOpened
+    }
+}
 
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    private var sections = [Section]()
+    
+   @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+
+        
+        sections = [
+        
+            Section(title: "Bikes", data: ["apache" ,"xblade", "xsr155"]),
+            Section(title: "cars", data: ["fortuner" ,"mercedez", "ford", "tata nexon"]),
+            Section(title: "Animals", data: ["cat" ,"mouse", "tiger", "lion"])
+            
+        ]
+      
     }
 
-
+    func numberOfSections(in tableView: UITableView) -> Int {
+       
+        return sections.count
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+     
+        let section = sections[section]
+        
+        if section.isOpened {
+            return section.data.count + 1
+        }else {
+            return 1
+        }
+     
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        if indexPath.row == 0 {
+            cell.textLabel?.text = sections[indexPath.section].title
+            cell.backgroundColor = .systemGreen
+            cell.layer.borderWidth = 2
+            cell.layer.borderColor = UIColor.systemBlue.cgColor
+        } else {
+            cell.textLabel?.text = sections[indexPath.section].data[indexPath.row - 1]
+            cell.backgroundColor = .systemGray3
+            cell.layer.borderWidth = 1
+            cell.layer.borderColor = UIColor.systemGreen.cgColor
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if indexPath.row == 0 {
+            
+            sections[indexPath.section].isOpened = !sections[indexPath.section].isOpened
+            tableView.reloadSections([indexPath.section], with: .none)
+            
+        } else {
+            print("inner cell tapped")
+        }
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.row == 0 {
+            return 80
+        }else {
+            return 60
+        }
+    }
 }
 
